e0e0c5c8419c88c114d0f55d51f61c1a8968b368
foreverealize <60579848+FOREVEREALIZE@users.noreply.github.com>
2025-01-10 20:56:33 +0100

    GoDocs and FromRGB

--- color.go
+++ color.go
@@ -1,31 +1,38 @@
 package candycane
 
+// Color represents a color. Direct access to color values should be done via methods.
 type Color struct {
 	red   int
 	green int
 	blue  int
 }
 
+// RGB returns three ints representing the red, green and blue channels of the color
 func (c Color) RGB() (int, int, int) {
 	return c.red, c.green, c.blue
 }
 
+// Red returns an int representing the red channel of the color
 func (c Color) Red() int {
 	return c.red
 }
 
+// Green returns an int representing the green channel of the color
 func (c Color) Green() int {
 	return c.green
 }
 
+// Blue returns an int representing the blue channel of the color
 func (c Color) Blue() int {
 	return c.blue
 }
 
+// Hex returns a string representing the hex color code of the color in the format "RRGGBB"
 func (c Color) Hex() string {
 	return intToAnyHex(c.red, 2) + intToAnyHex(c.green, 2) + intToAnyHex(c.blue, 2)
 }
 
+// HexHash returns a string representing the hex color code of the color in the format "#RRGGBB"
 func (c Color) HexHash() string {
 	return "#" + c.Hex()
 }
--- maker.go
+++ maker.go
@@ -2,6 +2,7 @@ package candycane
 
 import "strings"
 
+// FromHex takes a hex code as a string and returns a Color that represents it
 func FromHex(code string) Color {
 	cleanCode := strings.TrimPrefix(code, "#")
 	if len(cleanCode) != 6 {
@@ -12,5 +13,14 @@ func FromHex(code string) Color {
 		red:   doubleHexToInt(cleanCode[0:2]),
 		green: doubleHexToInt(cleanCode[2:4]),
 		blue:  doubleHexToInt(cleanCode[4:6]),
+	}
+}
+
+// FromRGB takes RGB values as three ints and returns a Color that represent them
+func FromRGB(red int, green int, blue int) Color {
+	return Color{
+		red:   red,
+		green: green,
+		blue:  blue,
 	}
 }
